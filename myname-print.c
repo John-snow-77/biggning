8b339347943e45ebb37259b48113c05df38c8bfd
Ankit Chaudhary <ac484709@gmail.com>
2026-01-10 11:01:47 +0530

    Add files via upload
    pattern printing

---
+++ myname-print.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    // Printing A
+    printf("    *    \n");
+    printf("   * *   \n");
+    printf("  *   *  \n");
+    printf("  *****  \n");
+    printf("  *   *  \n");
+    printf("  *   *  \n\n");
+
+    // Printing N
+    printf("  *   *  \n");
+    printf("  **  *  \n");
+    printf("  * * *  \n");
+    printf("  *  **  \n");
+    printf("  *   *  \n\n");
+
+    // Printing K
+    printf("  *   *  \n");
+    printf("  *  *   \n");
+    printf("  ***    \n");
+    printf("  *  *   \n");
+    printf("  *   *  \n\n");
+
+    // Printing I
+    printf("  *****  \n");
+    printf("    *    \n");
+    printf("    *    \n");
+    printf("    *    \n");
+    printf("  *****  \n\n");
+
+    // Printing T
+    printf("  *****  \n");
+    printf("    *    \n");
+    printf("    *    \n");
+    printf("    *    \n");
+    printf("    *    \n");
+
+    return 0;
+}
